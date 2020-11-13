423c1176dc0d7a68e66f63150a754aa66e7fe844
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-12 22:58:20 -0500

    feat: Observe main list

--- app/src/main/java/com/udacity/asteroid/radar/main/MainAdapter.kt
+++ app/src/main/java/com/udacity/asteroid/radar/main/MainAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroid.radar.R
 import com.udacity.asteroid.radar.databinding.ItemMainBinding
 import com.udacity.asteroid.radar.model.Asteroid
 
@@ -27,6 +28,14 @@ class MainAdapter : ListAdapter<Asteroid, MainAdapter.MainViewHolder>(DiffCallba
     class MainViewHolder(private var binding: ItemMainBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(asteroid: Asteroid) {
+            binding.titleTextView.text = asteroid.codename
+            binding.descriptionTextView.text = asteroid.closeApproachDate
+
+            if (asteroid.isPotentiallyHazardous) {
+                binding.statusImageView.setBackgroundResource(R.drawable.ic_status_potentially_hazardous)
+            } else {
+                binding.statusImageView.setBackgroundResource(R.drawable.ic_status_normal)
+            }
 //            binding.property = marsProperty
             // This is important, because it forces the data binding to execute immediately,
             // which allows the RecyclerView to make the correct view size measurements
