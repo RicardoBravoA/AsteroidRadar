f755ba7df2baa171b07d51f2a73119c8df21ff65
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-13 00:17:12 -0500

    refactor: Add styles

--- app/src/main/java/com/udacity/asteroid/radar/util/Constants.kt
+++ app/src/main/java/com/udacity/asteroid/radar/util/Constants.kt
@@ -2,7 +2,7 @@ package com.udacity.asteroid.radar.util
 
 object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
-    const val DEFAULT_END_DATE_DAYS = 7
+    const val DEFAULT_END_DATE_DAYS = 6
     const val BASE_URL = "https://api.nasa.gov/neo/rest/"
 
 }
