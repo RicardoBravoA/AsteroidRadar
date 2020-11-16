159e64839852f7d0e7520fd44ebabad6306736de
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-16 16:05:05 -0500

    refactor: Update detail adapter

--- app/src/main/java/com/udacity/asteroid/radar/detail/DetailAdapter.kt
+++ app/src/main/java/com/udacity/asteroid/radar/detail/DetailAdapter.kt
@@ -2,35 +2,59 @@ package com.udacity.asteroid.radar.detail
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.udacity.asteroid.radar.R
 import com.udacity.asteroid.radar.databinding.ItemDetailBinding
+import com.udacity.asteroid.radar.databinding.ItemDetailPictureBinding
 import com.udacity.asteroid.radar.model.DetailItem
+import com.udacity.asteroid.radar.util.bindingAdapter.bindImage
 import com.udacity.asteroid.radar.util.bindingAdapter.show
 
 class DetailAdapter(private val helpClick: () -> Unit) :
-    ListAdapter<DetailItem, DetailAdapter.DetailViewHolder>(DiffCallback) {
+    ListAdapter<DetailModel, RecyclerView.ViewHolder>(DiffCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
-        val binding: ItemDetailBinding = DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context), R.layout.item_detail, parent, false
-        )
-        return DetailViewHolder(binding, helpClick)
+    enum class Type {
+        PICTURE, ITEM
     }
 
-    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.bind(item)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            Type.PICTURE.ordinal -> PictureHolder.from(parent)
+            Type.ITEM.ordinal -> {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemDetailBinding.inflate(layoutInflater, parent, false)
+
+                return ItemViewHolder(binding, helpClick)
+            }
+            else -> throw ClassCastException("Unknown viewType $viewType")
+        }
     }
 
-    class DetailViewHolder(
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is PictureHolder -> {
+                val picture = getItem(position) as DetailModel.Picture
+                holder.bind(picture)
+            }
+            is ItemViewHolder -> {
+                val item = getItem(position) as DetailModel.Item
+                holder.bind(item.detailItem)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is DetailModel.Picture -> Type.PICTURE.ordinal
+            is DetailModel.Item -> Type.ITEM.ordinal
+        }
+    }
+
+    class ItemViewHolder(
         private var binding: ItemDetailBinding,
         private val helpClick: () -> Unit
-    ) :
-        RecyclerView.ViewHolder(binding.root) {
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(detailItem: DetailItem) {
             binding.titleTextView.text = detailItem.title
@@ -40,7 +64,23 @@ class DetailAdapter(private val helpClick: () -> Unit) :
             binding.helpImageView.setOnClickListener {
                 helpClick()
             }
+        }
+    }
 
+    class PictureHolder(private var binding: ItemDetailPictureBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(picture: DetailModel.Picture) {
+            binding.potentialImageView.bindImage(picture.url)
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): RecyclerView.ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemDetailPictureBinding.inflate(layoutInflater, parent, false)
+
+                return PictureHolder(binding)
+            }
         }
     }
 
@@ -50,7 +90,7 @@ class DetailAdapter(private val helpClick: () -> Unit) :
         }
 
         override fun areContentsTheSame(oldItem: DetailModel, newItem: DetailModel): Boolean {
-            return oldItem.title == newItem.title
+            return oldItem.id == newItem.id
         }
     }
 
