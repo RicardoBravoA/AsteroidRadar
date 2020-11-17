a3ed6987866e5c90fb63d35982b1ff190fede823
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-16 23:53:26 -0500

    feat: Add asteroid entity

---
+++ app/src/main/java/com/udacity/asteroid/radar/database/entity/AsteroidEntity.kt
@@ -0,0 +1,17 @@
+package com.udacity.asteroid.radar.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class AsteroidEntity(
+    @PrimaryKey
+    val id: Long,
+    val codename: String,
+    val closeApproachDate: String,
+    val absoluteMagnitude: Double,
+    val estimatedDiameter: Double,
+    val relativeVelocity: Double,
+    val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+)
