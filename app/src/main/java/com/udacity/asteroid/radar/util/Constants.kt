799c9a43ee10bbd6642f60b462d8fbc1b8fcb6cf
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-16 23:09:00 -0500

    feat: Update media type

--- app/src/main/java/com/udacity/asteroid/radar/util/Constants.kt
+++ app/src/main/java/com/udacity/asteroid/radar/util/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 6
     const val BASE_URL = "https://api.nasa.gov/"
     const val KEY = "oGWMd3Q0tO6xEMwTr38Kj74d84lJcQof2K1aD8sq"
+    const val PICTURE_TYPE = "image"
 
 }
