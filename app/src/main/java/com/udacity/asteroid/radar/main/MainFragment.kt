bdf0391e5d74b395c7b9a11ffee9c937135bdda6
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-17 23:28:20 -0500

    refactor: Remove logs

--- app/src/main/java/com/udacity/asteroid/radar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroid/radar/main/MainFragment.kt
@@ -1,7 +1,6 @@
 package com.udacity.asteroid.radar.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -54,17 +53,14 @@ class MainFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.show_week_asteroids -> {
-                Log.i("z- menu", "week")
                 mainViewModel.weekData()
                 return true
             }
             R.id.show_today_asteroids -> {
-                Log.i("z- menu", "today")
                 mainViewModel.today()
                 return true
             }
             R.id.show_saved_asteroids -> {
-                Log.i("z- menu", "saved")
                 mainViewModel.saved()
                 return true
             }
