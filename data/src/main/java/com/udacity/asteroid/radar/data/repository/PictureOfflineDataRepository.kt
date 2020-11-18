b6256b5efc1267ccfeec1c690e24cfbcfff21ff3
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-17 23:45:22 -0500

    feat: Add picture offline use case

--- data/src/main/java/com/udacity/asteroid/radar/data/repository/PictureOfflineDataRepository.kt
+++ data/src/main/java/com/udacity/asteroid/radar/data/repository/PictureOfflineDataRepository.kt
@@ -1,16 +1,19 @@
 package com.udacity.asteroid.radar.data.repository
 
-import com.udacity.asteroid.radar.data.datastore.PictureDataStoreFactory
+import android.content.Context
+import com.udacity.asteroid.radar.data.storage.PictureStorageDataStore
+import com.udacity.asteroid.radar.data.storage.database.AsteroidDatabase
 import com.udacity.asteroid.radar.domain.model.ErrorModel
 import com.udacity.asteroid.radar.domain.model.PictureModel
 import com.udacity.asteroid.radar.domain.repository.PictureRepository
 import com.udacity.asteroid.radar.domain.util.ResultType
 
-class PictureDataRepository(private val pictureDataStoreFactory: PictureDataStoreFactory) :
+class PictureOfflineDataRepository(private val context: Context) :
     PictureRepository {
 
     override suspend fun get(): ResultType<PictureModel, ErrorModel> {
-        val pictureDataStore = pictureDataStoreFactory.create()
-        return pictureDataStore.get()
+        val asteroidDatabase = AsteroidDatabase.getDatabase(context)
+        return PictureStorageDataStore(asteroidDatabase.asteroidDao).get()
     }
+
 }
--- domain/src/main/java/com/udacity/asteroid/radar/domain/usecase/PictureOfflineUseCase.kt
+++ domain/src/main/java/com/udacity/asteroid/radar/domain/usecase/PictureOfflineUseCase.kt
@@ -5,7 +5,7 @@ import com.udacity.asteroid.radar.domain.model.PictureModel
 import com.udacity.asteroid.radar.domain.repository.PictureRepository
 import com.udacity.asteroid.radar.domain.util.ResultType
 
-class PictureUseCase(private val pictureRepository: PictureRepository) {
+class PictureOfflineUseCase(private val pictureRepository: PictureRepository) {
 
     suspend fun get(): ResultType<PictureModel, ErrorModel> {
         return pictureRepository.get()
